085c9fcf47be82a95ce33de896826e2160bcc14f
Michael Wilson <mike@jumperstudios.com>
2023-01-22 16:19:32 -0600

    feat: created navigation menu component and added titles to pages

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import Header from "../components/header";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <Header />
         <Main />
+
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
