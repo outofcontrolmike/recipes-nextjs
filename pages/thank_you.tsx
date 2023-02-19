949400032ca6d93fe01a9d445ee268a35d1bba9a
Michael Wilson <outofcontrolmike@gmail.com>
2023-02-19 10:56:04 -0600

    wip: build issues

--- pages/thank_you.tsx
+++ pages/thank_you.tsx
@@ -1,9 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "../styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   // let d1 = new Date();
@@ -23,7 +18,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      <main>
         <h1>
           Thank you for sending us a message! We will get back to you when we
           can.
