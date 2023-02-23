1d30ee4b73bae840f8a51e78110bf217f260168b
Michael Wilson <mike@jumperstudios.com>
2023-02-23 09:48:44 -0600

    wip: fixing build again

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,8 +1,8 @@
 import { setupNextSanity } from 'next-sanity-extra';
 
 const config = {
-	projectId: process?.env?.NEXT_PUBLIC_SANITY_PROJECT_ID as string,
-	dataset: process?.env?.NEXT_PUBLIC_SANITY_DATASET as string,
+	projectId: "clj5orum",
+	dataset: "production",
 	useCdn: process?.env?.NODE_ENV === 'production',
 	token: process?.env.NEXT_PUBLIC_SANITY_API_TOKEN as string,
 };
