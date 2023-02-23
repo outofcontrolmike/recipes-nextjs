55416aada1bd41fd89615d3181f7ef9b8f753f6a
Michael Wilson <mike@jumperstudios.com>
2023-02-23 09:34:11 -0600

    wip: try to please build

--- lib/newSanity.ts
+++ lib/newSanity.ts
@@ -1,8 +1,8 @@
 import sanityClient from '@sanity/client';
 
 export const configuredSanityClient = sanityClient({
-	projectId: process?.env?.NEXT_PUBLIC_SANITY_PROJECT_ID,
-	dataset: process?.env?.NEXT_PUBLIC_SANITY_DATASET,
+	projectId: process?.env?.NEXT_PUBLIC_SANITY_PROJECT_ID as string,
+	dataset: process?.env?.NEXT_PUBLIC_SANITY_DATASET as string,
 	useCdn: true,
   apiVersion: '2022-05-22'
 });
